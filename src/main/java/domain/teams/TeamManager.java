040e338fbf2c8c71a4cda85ad3779b4c807d6a14
Simon <szhang0709@gmail.com>
2024-01-24 11:10:43 +0100

    function create team + test create team

--- src/main/java/domain/teams/TeamManager.java
+++ src/main/java/domain/teams/TeamManager.java
@@ -1,4 +1,16 @@
 package domain.teams;
 
-public class TeamManager {
+import domain.developers.Developer;
+
+import java.util.List;
+
+public class TeamManager implements ManageTeam {
+    private TeamRepository teamRepository;
+
+    @Override
+    public Team createTeam(String name, List<Developer> developers) {
+        Team team = new Team(name, developers);
+        teamRepository.createTeam(team);
+        return team;
+    }
 }
