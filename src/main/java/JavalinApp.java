53bd0996eb21697fa39231351c6f702862b36afb
Simon <szhang0709@gmail.com>
2024-03-01 21:29:30 +0100

    add: get next starting project

--- src/main/java/JavalinApp.java
+++ src/main/java/JavalinApp.java
@@ -37,7 +37,7 @@ public class JavalinApp {
         app.get("project/name/{name}", ProjectControllerAdapter::getProjectByName);
         app.get("project/all/", ProjectControllerAdapter::getAllProjects);
         app.get("project/status/{status}", ProjectControllerAdapter::getProjectsByStatus);
-//        app.get("project/nextStarting/", ProjectControllerAdapter::getNextStartingProject);
+        app.get("project/nextStarting/", ProjectControllerAdapter::getNextStartingProject);
 
         // POST
         app.post("project/", ProjectControllerAdapter::createProject);
